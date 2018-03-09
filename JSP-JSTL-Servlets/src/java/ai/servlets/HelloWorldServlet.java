2fac018d714b7cad1cc5c0077f3a2eb104e48953
tomi <tomi.git@gmail.com>
2018-03-09 22:20:07 +0100

    Implement form handling

--- JSP-JSTL-Servlets/src/java/ai/servlets/HelloWorldServlet.java
+++ JSP-JSTL-Servlets/src/java/ai/servlets/HelloWorldServlet.java
@@ -16,19 +16,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = {"/HelloWorldServlet"})
 public class HelloWorldServlet extends HttpServlet {
 
+    private static final String WINDOWS1250 = "windows-1250";
+    private static final String AGE = "age";
+    private static final String NAME = "name";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         response.setContentType("text/html");
-        response.setCharacterEncoding("windows-1250");
+        response.setCharacterEncoding(WINDOWS1250);
+        request.setCharacterEncoding(WINDOWS1250);
 
         PrintWriter out = response.getWriter();
+
+        String name = request.getParameter(NAME);
+        int age = Integer.valueOf(request.getParameter(AGE));
 
         out.println("<html>");
         out.println("<head><title>Hello World Servlet</title></head>");
         out.println("<body>");
         out.println("<h1>Hello World!</h1>");
+        out.println("<p>Witaj, " + name + ", masz " + age + " lat</p>");
         out.println("</body>");
         out.println("</html>");
         out.close();
