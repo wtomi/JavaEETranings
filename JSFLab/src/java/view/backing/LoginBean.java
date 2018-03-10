62954e82a5a59fddf3576bf335f477fa3b5aebfd
tomi <tomi.git@gmail.com>
2018-03-10 14:13:08 +0100

    Create checkbox for accepting service's terms

--- JSFLab/src/java/view/backing/LoginBean.java
+++ JSFLab/src/java/view/backing/LoginBean.java
@@ -7,6 +7,10 @@ package view.backing;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlSelectBooleanCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  *
@@ -15,22 +19,34 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "loginBean")
 @RequestScoped
 public class LoginBean {
-    
+
     private String username;
     private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckBox;
+    private HtmlCommandButton loginButton;
 
     /**
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
     }
-    
+
     public String login() {
-        if(username.equals(password)) {
+        if (getUsername().equals(getPassword())) {
             return "success.xhtml";
         } else {
             return "failure.xhtml";
         }
+    }
+
+    public void activateButton(ValueChangeEvent event) {
+        if (acceptCheckBox.isSelected()) {
+            loginButton.setDisabled(false);
+        } else {
+            loginButton.setDisabled(true);
+        }
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
     }
 
     /**
@@ -60,5 +76,33 @@ public class LoginBean {
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
+    /**
+     * @return the acceptCheckBox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckBox() {
+        return acceptCheckBox;
+    }
+
+    /**
+     * @param acceptCheckBox the acceptCheckBox to set
+     */
+    public void setAcceptCheckBox(HtmlSelectBooleanCheckbox acceptCheckBox) {
+        this.acceptCheckBox = acceptCheckBox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
 }
