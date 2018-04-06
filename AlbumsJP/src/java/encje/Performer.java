3d8f23b4c34d70a7e9811fc0896ceb9e499afb35
tomi <tomi.git@gmail.com>
2018-04-06 19:37:14 +0200

    Modify Performer Entity

--- AlbumsJP/src/java/encje/Performer.java
+++ AlbumsJP/src/java/encje/Performer.java
@@ -8,19 +8,21 @@ package encje;
 import java.io.Serializable;
 import java.util.Collection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 /**
  *
  * @author Tomi
  */
 @Entity
-@Table(name = "WYKONAWCY")
-class Performer implements Serializable {
+public class Performer implements Serializable {
 
+    private static long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
     private String name;
@@ -34,6 +36,45 @@ class Performer implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getId() != null ? getId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Performer)) {
+            return false;
+        }
+        Performer other = (Performer) object;
+        if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "encje.Performer[ id=" + getId() + " ]";
+    }
+
+    /**
+     * @return the serialVersionUID
+     */
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @param aSerialVersionUID the serialVersionUID to set
+     */
+    public static void setSerialVersionUID(long aSerialVersionUID) {
+        serialVersionUID = aSerialVersionUID;
     }
 
     /**
@@ -63,5 +104,5 @@ class Performer implements Serializable {
     public void setAlbums(Collection<Album> albums) {
         this.albums = albums;
     }
-
+    
 }
