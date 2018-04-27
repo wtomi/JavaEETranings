d76acd30de54714dad8f65d8fe4437568603b1f6
student <student@Lab-dbm-16.CSPUT>
2018-04-27 10:49:55 +0200

    Add requests in client

--- ComplaintsClient/src/complaintsclient/ComplaintsClient.java
+++ ComplaintsClient/src/complaintsclient/ComplaintsClient.java
@@ -7,7 +7,9 @@ package complaintsclient;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -27,6 +29,41 @@ public class ComplaintsClient {
                         .get(String.class);
 
         System.out.println("Count: " + count);
+
+        String allComplaints = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints")
+                .request(MediaType.APPLICATION_XML)
+                .get(String.class);
+
+        System.out.println("\n\n" + allComplaints);
+
+        String oneComplaint = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints/2")
+                .request(MediaType.APPLICATION_XML)
+                .get(String.class);
+
+        System.out.println("\n\n" + oneComplaint);
+
+        //String modified = oneComplaint.replace("open", "closed");
+        String modified = oneComplaint.replace("closed", "open");
+
+        Response response
+                = client.target("http://localhost:8080/Complaints/"
+                        + "resources/complaints/2")
+                        .request()
+                        .put(Entity.entity(modified, MediaType.APPLICATION_XML));
+
+        String allOpen = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints")
+                .request()
+                .get(String.class);
+        
+        oneComplaint = client.target("http://localhost:8080/Complaints/"
+                + "resources/complaints/2")
+                .request(MediaType.APPLICATION_XML)
+                .get(String.class);
+
+        System.out.println("\n\n" + oneComplaint);
 
         client.close();
     }
